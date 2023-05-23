ccfd1c20744632a3ab10ec6140840113b9bb4f4e
Oleksandr Barsuk <sbarsuk88@gmail.com>
2023-05-23 18:29:09 +0300

    Added console options, refactored parsers

---
+++ src/InfoSearch/InfoSearch.App/ConsoleOptions.cs
@@ -0,0 +1,10 @@
+﻿using InfoSearch.Core;
+using System.Reflection;
+
+namespace InfoSearch.App;
+
+internal class ConsoleOptions
+{
+    public ParserType Type { get; set; } = ParserType.EPUB;
+    public string WorkingDirectory { get; set; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "\\";
+}
--- src/InfoSearch/InfoSearch.App/Program.cs
+++ src/InfoSearch/InfoSearch.App/Program.cs
@@ -1,37 +1,40 @@
-﻿using InfoSearch.Core;
+﻿using InfoSearch.App;
+using InfoSearch.Core;
 using InfoSearch.Parsing;
 using InfoSearch.Serializing;
-using System.Reflection;
 
-Console.WriteLine("InfoSearch Dictionary");
-
-// var currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "\\";
-var downloadDirectory = "C:\\Users\\Oleksandr_Barsuk\\Downloads";
-
-var searchPattern = "*.epub"; // "*.pdf";
-
-var documents = FileScanner.Scan(downloadDirectory, searchPattern);
-Console.WriteLine($"Documents: \n - {string.Join("\n - ", documents)}.");
-
-var dictionary = new Dictionary();
-var parser = new EpubParser();//PdfParser();
-
-foreach (var document in documents)
+public class Program
 {
-    var doc = parser.Parse(document);
-    var terms = doc.SplitTerms();
-    dictionary.AddRange(terms);
+    private static ParserResolver parserResolver = new ParserResolver();
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("InfoSearch Dictionary");
+
+        var options = ArgsParser.Parse(args);
+        var parser = parserResolver.Resolve(options.Type);
+
+        var documents = FileScanner.Scan(options.WorkingDirectory, parser.SearchPattern);
+        Console.WriteLine($"Documents: \n - {string.Join("\n - ", documents)}.");
+
+        var dictionary = new Dictionary();
+
+        foreach (var document in documents)
+        {
+            var doc = parser.Parse(document);
+            var terms = doc.SplitTerms();
+            dictionary.AddRange(terms);
+        }
+
+        Console.WriteLine($"Result set: {dictionary}");
+
+        Console.WriteLine($"Dictionary length: {dictionary.Set.Count()}");
+
+        var currentTime = DateTime.Now;
+        var filename = $"{options.WorkingDirectory}\\{currentTime.ToString("yyyy-MM-dd-HH-mm-ss")}_dictionary.txt";
+
+        dictionary.Save(new TextSerializer(), filename);
+
+        Console.WriteLine("Done...");
+    }
 }
-
-Console.WriteLine($"Result set: {dictionary}");
-
-Console.WriteLine($"Dictionary length: {dictionary.Set.Count()}");
-
-var currentTime = DateTime.Now;
-var filename = $"{downloadDirectory}\\{currentTime.ToString("yyyy-MM-dd-HH-mm-ss")}_dictionary.txt";
-
-dictionary.Save(new TextSerializer(), filename);
-
-Console.WriteLine("Done...");
-
-Console.ReadLine();
