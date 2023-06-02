add33ba9da54899401adfd666c5e0fd1b52a7851
Oleksandr Barsuk <sbarsuk88@gmail.com>
2023-06-02 16:05:25 +0300

    added stopwatch

--- src/InfoSearch/Executable/InfoSearch.IndexApp/Program.cs
+++ src/InfoSearch/Executable/InfoSearch.IndexApp/Program.cs
@@ -10,20 +10,23 @@ namespace InfoSearch.IndexApp;
 
 internal class Program
 {
-    private static ParserResolver parserResolver = new ParserResolver();
-    private static string[] exitCommands = { "exit", "quit", "e", "q" };
+    private static ParserResolver _parserResolver = new ParserResolver();
+    private static string[] _exitCommands = { "exit", "quit", "e", "q" };
+    private static StopWatch _watch = new StopWatch();
 
     static void Main(string[] args)
     {
         Console.WriteLine("InfoSearch Indexes");
 
         var options = ArgsParser.Parse(args);
-        var parser = parserResolver.Resolve(options.Type);
+        var parser = _parserResolver.Resolve(options.Type);
 
         var documents = FileScanner.Scan(options.WorkingDirectory, parser.SearchPattern);
         Console.WriteLine($"Documents: \n - {string.Join("\n - ", documents)}.");
 
         var documentList = new List<Document>();
+
+        _watch.Start();
 
         foreach (var document in documents)
         {
@@ -31,24 +34,33 @@ internal class Program
             documentList.Add(new Document(document, parsedText));
         }
 
-        //var index = new StaticIncidenceMatrix(documentList);
-        //var queryRunner = new IncidenceMatrixQueryRunner(index);
+        _watch.Stop();
+        _watch.Print("Parsing documents");
 
-        var index = new InvertedListIndex(documentList);
-        var queryRunner = new InvertedIndexQueryRunner(index);
+        _watch.Start();
+        var index = new StaticIncidenceMatrix(documentList);
+        var queryRunner = new IncidenceMatrixQueryRunner(index);
 
-        string queryString = string.Empty;
+        //var index = new InvertedListIndex(documentList);
+        //var queryRunner = new InvertedIndexQueryRunner(index);
+
+        _watch.Stop();
+        _watch.Print("Creating index");
 
         Console.WriteLine("Please enter your query:");
-        queryString = Console.ReadLine() ?? string.Empty;
+        string queryString = Console.ReadLine() ?? string.Empty;
 
-        while (!exitCommands.Contains(queryString))
+        while (!_exitCommands.Contains(queryString))
         {
             if (string.IsNullOrEmpty(queryString))
                 continue;
 
             var query = QueryParser.Parse(queryString);
+
+            _watch.Start();
             var searchResults = queryRunner.Run(query);
+            _watch.Stop();
+            _watch.Print("Searching documents");
 
             Console.WriteLine($"Search results for query \"{queryString}\":\n - {string.Join("\n - ", searchResults)}.");
 
---
+++ src/InfoSearch/InfoSearch.ConsoleUtils/StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace InfoSearch.ConsoleUtils;
+
+public class StopWatch
+{
+    private readonly Stopwatch _watch = new Stopwatch();
+
+    public void Start()
+    {
+        if (_watch.IsRunning)
+            throw new Exception("Current StopWatch is running; it should be stopped first.");
+        
+        _watch.Reset();
+        _watch.Start();
+    }
+
+    public void Stop() => _watch.Stop();
+
+    public void Print(string text)
+    {
+        Console.WriteLine($"[StopWatch]: {text} execution time: {_watch.ElapsedMilliseconds} ms");
+    }
+}
