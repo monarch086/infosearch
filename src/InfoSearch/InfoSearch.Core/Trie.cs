ad5c9c00957adcfbb092b04bc448d1e4304a27d9
Oleksandr Barsuk <sbarsuk88@gmail.com>
2023-06-16 23:45:18 +0300

    added trie

---
+++ src/InfoSearch/InfoSearch.Core/Trie.cs
@@ -0,0 +1,90 @@
+﻿namespace InfoSearch.Core;
+
+public class Trie
+{
+    private TrieNode root;
+
+    public Trie()
+    {
+        root = new TrieNode();
+    }
+
+    public void Insert(string word)
+    {
+        var current = root;
+
+        foreach (char c in word)
+        {
+            if (!current.Children.ContainsKey(c))
+            {
+                current.Children[c] = new TrieNode();
+            }
+
+            current = current.Children[c];
+        }
+
+        current.IsWordEnd = true;
+    }
+
+    public bool Contains(string word)
+    {
+        TrieNode current = root;
+
+        foreach (char c in word)
+        {
+            if (!current.Children.ContainsKey(c))
+            {
+                return false;
+            }
+
+            current = current.Children[c];
+        }
+
+        return current.IsWordEnd;
+    }
+
+    public IList<string> StartsWith(string prefix)
+    {
+        var results = new List<string>();
+        var current = root;
+
+        foreach (char c in prefix)
+        {
+            if (!current.Children.ContainsKey(c))
+            {
+                return results;
+            }
+
+            current = current.Children[c];
+        }
+
+        CollectWords(current, prefix, results);
+
+        return results;
+    }
+
+    private void CollectWords(TrieNode node, string prefix, List<string> results)
+    {
+        if (node.IsWordEnd)
+        {
+            results.Add(prefix);
+        }
+
+        foreach (char c in node.Children.Keys)
+        {
+            CollectWords(node.Children[c], prefix + c, results);
+        }
+    }
+}
+
+internal class TrieNode
+{
+    public IDictionary<char, TrieNode> Children { get; set; }
+    public bool IsWordEnd { get; set; }
+
+    public TrieNode()
+    {
+        Children = new Dictionary<char, TrieNode>();
+        IsWordEnd = false;
+    }
+}
---
+++ src/InfoSearch/InfoSearch.QueryProcessing/QueryRunners/TrieQueryRunner.cs
@@ -0,0 +1,49 @@
+﻿using InfoSearch.Core;
+using InfoSearch.Core.Extensions;
+using InfoSearch.Core.Model;
+
+namespace InfoSearch.QueryProcessing.QueryRunners;
+
+public class TrieQueryRunner
+{
+    private readonly Trie directTrie = new Trie();
+    private readonly Trie reverseTrie = new Trie();
+
+    private const char JOKER_SIGN = '*';
+
+    public TrieQueryRunner(IEnumerable<Document> documents)
+    {
+        foreach (var document in documents)
+        {
+            var terms = document.Text.SplitTerms();
+            foreach (var term in terms)
+            {
+                directTrie.Insert(term);
+                reverseTrie.Insert(RotateWord(term));
+            }
+        }
+    }
+
+    public IEnumerable<string> Run(string query)
+    {
+        if (query.Count(c => c == JOKER_SIGN) != 1)
+            throw new ArgumentException($"Input query should contain one joker sign ({JOKER_SIGN}).");
+
+        var queryParts = query.Split(JOKER_SIGN);
+
+        var beforeJokerWords = directTrie.StartsWith(queryParts[0]);
+        var afterJokerWords = reverseTrie.StartsWith(RotateWord(queryParts[1]))
+            .Select(w => RotateWord(w));
+
+        if (string.IsNullOrEmpty(queryParts[0]))
+            return afterJokerWords;
+        else if (string.IsNullOrEmpty(queryParts[1]))
+            return beforeJokerWords;
+        else return beforeJokerWords.Intersect(afterJokerWords);
+    }
+
+    private string RotateWord(string word)
+    {
+        return string.Join(null, word.Reverse());
+    }
+}
